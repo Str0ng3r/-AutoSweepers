ce2bac5ac4ef87cd0375aa298bb30d82fc7f965a
Maks <116167012+Str0ng3r@users.noreply.github.com>
2024-02-21 23:05:38 +0100

    updated

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -282,7 +282,7 @@ export const Main = () => {
               <small>грн.</small>
             </div>
           </div>
-          <div className={styles.zakaz_cont} id="zakaz">
+          <div className={styles.zakaz_cont} id="zakaz"> 
             <input
               type="text"
               placeholder="Ваше ім'я"
